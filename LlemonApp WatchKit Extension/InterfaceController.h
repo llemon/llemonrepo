bdff15e0bc2daf8bfb4c1383459661cf4a417a6f
Tack-gyu Lee <spinion13@gmail.com>
2015-06-12 07:13:28 +0900

    initial commit

---
+++ LlemonApp WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  LlemonApp WatchKit Extension
+//
+//  Created by 이택규 on 2015. 6. 12..
+//  Copyright (c) 2015년 Llemon. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
