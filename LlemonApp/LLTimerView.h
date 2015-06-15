f128dd71b9c9c3e1220f90a8a2076a5db0ac0959
Tack-gyu Lee <spinion13@gmail.com>
2015-06-16 00:46:15 +0900

    타이머 복구

---
+++ LlemonApp/LLTimerView.h
@@ -0,0 +1,13 @@
+//
+//  LLTimerView.h
+//  LlemonApp
+//
+//  Created by 이택규 on 2015. 6. 15..
+//  Copyright (c) 2015년 Llemon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LLTimerView : UIView
+
+@end
---
+++ LlemonApp/LLTimerView.m
@@ -0,0 +1,26 @@
+//
+//  LLTimerView.m
+//  LlemonApp
+//
+//  Created by 이택규 on 2015. 6. 15..
+//  Copyright (c) 2015년 Llemon. All rights reserved.
+//
+
+#import "LLTimerView.h"
+
+@interface LLTimerView ()
+
+
+@end
+
+@implementation LLTimerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
